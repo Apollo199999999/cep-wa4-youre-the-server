e8f5a68ce94f878e938f61b14c85cae0684bf1b1
Matthias Wang <matthiaswang60@gmail.com>
2024-05-31 17:55:22 +0800

    Initial commit with working start screen

---
+++ README.md
@@ -0,0 +1,3 @@
+# cep-wa4-youre-the-server
+* Portfolio link: https://matthiaswang.notion.site/CEP-WA4-Ping-Pong-21df4c584cd64c138a3dffa4851a27f0?pvs=74
+* If running locally, run `npm run server` in the root directory.
---
+++ index.js
@@ -0,0 +1,13 @@
+function startGame() {
+    // Navigate to sketch.html
+    window.location.href = "sketch.html";
+}
+
+function howToPlay() {
+    // TODO
+}
+
+function aboutGame() {
+    // TODO
+    // Remember to link to source code, portfolio, and icons8 fluency pages in the about section
+}
