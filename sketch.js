ff8b6feaf2ea1dfde0ed873a8eda747dce328ad0
Matthias Wang <matthiaswang60@gmail.com>
2024-05-31 19:01:04 +0800

    Fix sketch aspect ratio

--- sketch.js
+++ sketch.js
@@ -1,9 +1,11 @@
-let ball;
+// Fix the sketch size to 16:9 so we can scale the sprites on different screen sizes
+const SKETCH_WIDTH = Math.min(document.documentElement.clientWidth, document.documentElement.clientHeight * 16 / 9);
+const SKETCH_HEIGHT = Math.min(document.documentElement.clientHeight, document.documentElement.clientWidth * 9 / 16);
 
 function setup() {
-	new Canvas(500, 500);
+	new Canvas(SKETCH_WIDTH, SKETCH_HEIGHT);
 
-	ball = new Sprite();
+	let ball = new Sprite();
 	ball.diameter = 50;
 }
 
