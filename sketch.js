265c8b10aae35c6342fc9bcab2f85cb1c8706658
Matthias Wang <matthiaswang60@gmail.com>
2024-06-07 23:16:54 +0800

    Some facsimile of a toolbar

--- sketch.js
+++ sketch.js
@@ -23,6 +23,9 @@ let roomsCollection;
 // Array of valid room codes that the clients can take (player will create rooms based on these codes)
 let validRoomCodes = ["19283", "58124", "92641", "38201", "10358", "72912"];
 
+// iframe to display toolbar
+let toolbarFrame;
+
 function preload() {
 	// Preload resources
 	clientHappyImg = loadImage("../images/client/happy.png");
@@ -40,12 +43,12 @@ function setup() {
 	randomWordClass = new RandomWords();
 
 	// Create test room and client sprites so we can get their width
-	let testClientSprite = new Client("Test", 
-		validRoomCodes[Math.floor(random(0, validRoomCodes.length))], 
-		null, 
-		clientHappyImg, 
-		clientIrritatedImg, 
-		clientAngryImg, 
+	let testClientSprite = new Client("Test",
+		validRoomCodes[Math.floor(random(0, validRoomCodes.length))],
+		null,
+		clientHappyImg,
+		clientIrritatedImg,
+		clientAngryImg,
 		interNormal);
 
 	// Area where new clients are spawned
@@ -55,7 +58,13 @@ function setup() {
 	roomsCollection = new SpriteCollection(width * 0.04 + newClientsCollection.w, height * 0.02, 2, 3, testRoomSprite.w, testRoomSprite.h, '#', "Rooms", interBold);
 
 	testClientSprite.remove();
-	testRoomSprite.removeAllSprites()
+	testRoomSprite.removeAllSprites();
+
+	// Create a iframe to display toolbar
+	toolbarFrame = createElement('iframe').size(width - width * 0.06 - newClientsCollection.w - roomsCollection.w, height);
+	toolbarFrame.attribute('frameBorder', '0');
+	toolbarFrame.attribute('src', './ui/toolbar.html');
+	toolbarFrame.position((document.documentElement.clientWidth - width) / 2 + width * 0.06 + newClientsCollection.w + roomsCollection.w, 0);
 }
 
 function draw() {
@@ -65,20 +74,20 @@ function draw() {
 		// Add a new client
 		// Username should be max 6 chars long
 		let username = capitalizeFirstLetter(randomWordClass.generateWordWithMaxLength(4)) + Math.floor(random(10, 100));
-		let newClient =  
-		new Client(username, 
-					validRoomCodes[Math.floor(random(0, validRoomCodes.length))], 
-					newClientsCollection, 
-					clientHappyImg, 
-					clientIrritatedImg, 
-					clientAngryImg, 
-					interNormal);
+		let newClient =
+			new Client(username,
+				validRoomCodes[Math.floor(random(0, validRoomCodes.length))],
+				newClientsCollection,
+				clientHappyImg,
+				clientIrritatedImg,
+				clientAngryImg,
+				interNormal);
 
-		newClientsCollection.push(newClient);	
+		newClientsCollection.push(newClient);
 	}
 }
 
 // Helper functions
 function capitalizeFirstLetter(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
+	return string.charAt(0).toUpperCase() + string.slice(1);
 }
