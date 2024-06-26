d0b7066d30c911efe677e4e809ac3548979a206e
Matthias Wang <matthiaswang60@gmail.com>
2024-06-26 18:34:08 +0800

    Disable friendly errors to reduce lag

--- sketch.js
+++ sketch.js
@@ -1,3 +1,6 @@
+// Disable the FES to reduce lag
+p5.disableFriendlyErrors = true;
+
 // Fix the sketch size to 16:9 so we can scale the sprites on different screen sizes
 const SKETCH_WIDTH = Math.min(document.documentElement.clientWidth, document.documentElement.clientHeight * 16 / 9);
 const SKETCH_HEIGHT = Math.min(document.documentElement.clientHeight, document.documentElement.clientWidth * 9 / 16);
